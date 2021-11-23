8cf11ac8de291fafcc7b27fe4e1e143047b06168
Simon Szustkowski <mail@simonszu.de>
2021-11-23 09:43:20 +0100

    Recipe is now properly formed

--- webview.js
+++ webview.js
@@ -6,13 +6,18 @@ module.exports = Ferdi => {
     // title is like (2) Circuit where 2 is the number of unread messages, if any
     // if there is no unread message, title is only 'Circuit'
     // we need to extract the title value and do some regex stuff
-    // regex is '\((\d+)\) Circuit'
+    // regex is '\((\d+)\) Circuit'#
+
+    // Get value of <title> tag 
     const titleValue = document.querySelector('title').text;
+
+    // Define Regex
     const regEx = new RegExp("\((\d+)\) Circuit"); 
 
+    
     match = regEx.exec(titleValue);
-    if (regEx.test(titleValue)) {
-      unread = 
+    if (match.length != 0) {
+      unread = match[0];
     }
     Ferdi.setBadge(parseInt(unread, 10));
   };
